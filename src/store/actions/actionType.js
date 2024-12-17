7f588624084aa29dd53b0591538381d97b0053f2
teceudesenvolvo <79342596+teceudesenvolvo@users.noreply.github.com>
2024-12-17 09:45:55 -0300

    01

--- src/store/actions/actionType.js
+++
@@ -1,5 +0,0 @@
-export const OPEN_PRODUCT = 'OPEN_PRODUCT'
-export const OPEN_SERVICE = 'OPEN_SERVICE'
-export const VISIT_REGISTER = 'VISIT_REGISTER'
-export const USER_LOGGED_IN = 'USER_LOGGED_IN'
-export const USER_LOGGED_OUT = 'USER_LOGGED_OUT'
--- src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { clickReducer } from './clickReducer';
-import userReducer from './userReducer'
-import { combineReducers } from 'redux';
-
-export const Reducers = combineReducers({
-  service: clickReducer,
-  user: userReducer
-});
